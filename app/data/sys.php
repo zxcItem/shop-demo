4f3f46a1790a044f833d30e5904c4cf6c22fe9bc
叶落无痕 <1262847141@qq.com>
2026-04-12 11:53:08 +0800

    模板修复

--- app/data/sys.php
+++ app/data/sys.php
@@ -19,9 +19,8 @@ if (Library::$sapp->request->isCli()) {
 
 if (!function_exists('show_gspec')) {
     /**
-     * 商品规格过滤显示
+     * 商品规格过滤显示.
      * @param string $spec 原规格内容
-     * @return string
      */
     function show_gspec(string $spec): string
     {
@@ -32,17 +31,17 @@ if (!function_exists('show_gspec')) {
         return join(' ', $specs);
     }
 }
+
 if (!function_exists('formatdate')) {
     /**
-     * 日期格式过滤
-     * @param string|null $value
-     * @return string|null
+     * 日期格式过滤.
      */
     function formatdate(?string $value): ?string
     {
         return is_string($value) ? str_replace(['年', '月', '日'], ['-', '-', ''], $value) : $value;
     }
 }
+
 
 if (!function_exists('processdate')) {
     /**
